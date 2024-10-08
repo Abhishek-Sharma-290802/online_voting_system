1b72ca5bfc42dd1f092978234feeeb4db3485eae
Abhishek Sharma <121885746+Abhishek-Sharma-290802@users.noreply.github.com>
2024-10-08 16:44:21 +0530

    Update conn.php

--- includes/conn.php
+++ includes/conn.php
@@ -1,8 +1,16 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'votesystem');
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:votesystem-db-server.database.windows.net,1433; Database = votesystem", "azure", "abhi@123");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
-	
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "azure", "pwd" => "abhi@123", "Database" => "votesystem", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:votesystem-db-server.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
